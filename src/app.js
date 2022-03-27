61e42ffbf69eecc7e799e8fe268653a5e8c177bb
renanfelipelopes <renanlopes.ads@gmail.com>
2022-03-27 19:46:13 -0300

    Inputs e Data Binding

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 const MyNameApp = {
   data() {
     return {
-      name: "Renan",
-			age: 25
-    };
+      name: "",
+			age: 25,
+      input_name: ""
+    }
   },
+  methods: {
+    submitForm(e) {
+      e.preventDefault();
+      console.log(this.input_name);
+      this.name = this.input_name;
+    }
+  }
 };
 
 Vue.createApp(MyNameApp).mount("#app");
